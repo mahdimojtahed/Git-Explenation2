9e9919e2bf3beb47ae2980e4086a7679e0842986
medev-lennovo <mahdyarmojtahed@gmail.com>
2024-05-22 16:08:46 +0330

    Initial Commit

---
+++ src/main/java/com/dotin/git/GitExplenation2Application.java
@@ -0,0 +1,13 @@
+package com.dotin.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExplenation2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitExplenation2Application.class, args);
+    }
+
+}
